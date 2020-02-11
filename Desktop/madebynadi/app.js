754a94656f76b9ddb3cd4f3365de631bf52b5112
nadihsuwai <hsuwainadi86@gmail.com>
2020-02-11 14:25:31 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -125,11 +125,10 @@ function handleMessage(sender_psid, received_message) {
   let response;
   
   // Checks if the message contains text
-  if (received_message.text == "cl") {    
+  if (received_message.text == "choose location") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": "chooes location",
       "quick_replies":[
       {
         "content_type":"text",
