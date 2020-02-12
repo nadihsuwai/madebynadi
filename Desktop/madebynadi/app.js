eb5ec51247ea1f985347b0a5cd4620aabe069561
nadihsuwai <hsuwainadi86@gmail.com>
2020-02-12 08:45:51 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -139,6 +139,13 @@ function handleMessage(sender_psid, received_message) {
       "text": 'Thank you',
     }
   }
+   else if (received_message.text == "No!") {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": 'OK',
+    }
+  }
   else if (received_message.text == "home") {
     response = {
       "text":'please send me details address'
