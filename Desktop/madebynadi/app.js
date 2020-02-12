ee9a93083cee899245ba26ecdcf7310b268c6fed
nadihsuwai <hsuwainadi86@gmail.com>
2020-02-12 12:24:03 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -161,16 +161,16 @@ function handleMessage(sender_psid, received_message) {
                     "title":"Cancle",
                     "payload":"IWC"
                   }]
-               (received_message.text == "Order") {    
+
+    }
+  }
+else if (received_message.text == "Order") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": 'Thank you for your making by mypage.',
-    }
-  }   
+      "text": 'Thank for your order comfirm and make by mypage.',
     }
   }
-
    else if (received_message.text == "Shop") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
@@ -186,17 +186,16 @@ function handleMessage(sender_psid, received_message) {
                     "title":"Cancle",
                     "payload":"IWC"
                   }]
-                  (received_message.text == "Order") {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": 'Thank you for your making by mypage.',
-    }
-  }
 
     }
   }
- 
+ else if (received_message.text == "Order") {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": 'Thank for your order comfirm and make by mypage.',
+    }
+  }
 
   
   else if (received_message.text == "home") {
