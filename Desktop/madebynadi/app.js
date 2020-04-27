e81befe85d56f1baed10db6e8309a2c2fafbf101
nadihsuwai <hsuwainadi86@gmail.com>
2020-04-27 22:01:47 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -272,7 +272,7 @@ function handleMessage(sender_psid, received_message) {
 
     }
   }
-e if (received_message.text == "Order") {   
+ if (received_message.text == "Order") {   
 
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
