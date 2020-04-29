f16b4916f071373b9c70ec465182f4203ef641cd
nadihsuwai <hsuwainadi86@gmail.com>
2020-04-29 19:29:16 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -639,7 +639,6 @@ else if (received_message.text=== 'It is 4 am') {
     response = {
       "text": 'please send me details address',
     }
-
   } 
  else  if (received_message.text == "No") {    
     // Create the payload for a basic text message, which
@@ -768,8 +767,7 @@ else if (received_message.text == "Customer Home") {
     response = {
       "text": 'please send me details address',
     }
-     
-  } 
+   } 
  else if (received_message.text == "No") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
