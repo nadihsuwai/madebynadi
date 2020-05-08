bb610f799c40c6eb0b2853928c518cd3bb3e4f59
nadihsuwai <hsuwainadi86@gmail.com>
2020-05-08 10:25:30 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -39,7 +39,7 @@ var firebaseConfig = {
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "project_id": process.env.FIREBASE_PROJECT_ID,    
     }),
-    databaseURL: "https://madebynadi-92319.firebaseio.com", 
+    databaseURL: process.env.FIREBASE_DB_URL, 
     
   };
 
