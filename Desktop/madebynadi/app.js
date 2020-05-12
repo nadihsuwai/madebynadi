8e08dd355636425623c47221d16c252ea6c27a9e
nadihsuwai <hsuwainadi86@gmail.com>
2020-05-12 20:05:12 +0630

    renew

--- Desktop/madebynadi/app.js
+++ Desktop/madebynadi/app.js
@@ -147,18 +147,7 @@ var make = {
   cusphnum:false,
     };
 function handleMessage(sender_psid, received_message) {
-   const saveData_BK = (sender_psid) => {
-  const bk_info = {
-    id : sender_psid,
-    bookingdate : userEnteredInfo.bookingdate,
-    bdk : userEnteredInfo.bdk,
-    cusaddress : userEnteredInfo.cusaddress,
-    cusphnum : userEnteredInfo.cusphnum,
-  }
-  db.collection('Customer_info').add(bk_info);
-}
-
-
+   
   let response;
 
   
